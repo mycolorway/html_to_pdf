d7dc037f7c5b86646e1185c7b0f26caeb8f517ab
Song Hui <ifournight@gmail.com>
2019-01-16 16:26:49 +0800

    CHG: update build.sh run.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-# docker build --build-arg HTTP_PROXY=172.20.10.2:6152 --build-arg HTTPS_PROXY=172.20.10.2:6152 -t tower2/html_to_pdf .
-docker build --no-cache -t tower2/html_to_pdf .
-# docker build -t tower2/html_to_pdf .
+docker build --no-cache -t ifournight/html-to-pdf .
+docker push ifournight/html-to-pdf
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 echo "Running tower2/html_to_pdf"
 
-docker run -i --rm --env PORT=3002 --cap-add=SYS_ADMIN -p 3002:3002/tcp --name html_to_pdf tower2/html_to_pdf
+docker run -i --rm --env PORT=3001 --cap-add=SYS_ADMIN -p 3001:3001/tcp --name html-to-pdf ifournight/html-to-pdf
+# docker run -d -i --rm --env PORT=3001 --cap-add=SYS_ADMIN -p 3001:3001/tcp --name html-to-pdf ifournight/html-to-pdf
