7b022171494308f41785906e45aefe68aaf5fbcd
Song Hui <ifournight@gmail.com>
2019-01-16 16:19:41 +0800

    CHG: set_content 添加参数 "waitUntil": "networkidle0"
    这样 pdf 会等到页面所有的请求（比如图片）结束后才开始渲染
    参考资料： https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagegotourl-options

--- app.js
+++ app.js
@@ -4,32 +4,38 @@ const puppeteer = require('puppeteer')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3001
 
-app.use(bodyParser.text({ limit: '2mb' }))
+app.use(bodyParser.text({
+  limit: '2mb'
+}))
 
 app.post('/', function(req, res) {
-    // var contentType = req.headers['content-type']
+  // var contentType = req.headers['content-type']
 
-    (async() => {
-        try {
-            const browser = await puppeteer.launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
-            })
-    
-            const page = await browser.newPage()
-            await page.setContent(req.body)
-            await page.pdf({ format: 'A4' }).then(function(buffer) {
-                res.setHeader('Content-Disposition', 'attachment;filename="default.pdf"')
-                res.setHeader('Content-Type', 'application/pdf')
-                res.send(buffer)
-            })
-    
-            await browser.close()
-        } catch (error) {
-            console.error(error);
-        }
-    })()
+  (async() => {
+    try {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+      })
+
+      const page = await browser.newPage()
+      await page.setContent(req.body, {
+        "waitUntil": "networkidle0"
+      })
+      await page.pdf({
+        format: 'A4'
+      }).then(function(buffer) {
+        res.setHeader('Content-Disposition', 'attachment;filename="default.pdf"')
+        res.setHeader('Content-Type', 'application/pdf')
+        res.send(buffer)
+      })
+
+      await browser.close()
+    } catch (error) {
+      console.error(error);
+    }
+  })()
 })
 
 app.listen(port, function() {
-    console.log('App listening on port ' + port)
+  console.log('App listening on port ' + port)
 })
