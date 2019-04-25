825cffc3eb8d2ade78f23328ccce790785808173
yxwyoyoyo <yxwyoyoyo@gmail.com>
2019-04-25 11:13:04 +0800

    DEL: useless code

--- run.sh
+++
@@ -1,4 +0,0 @@
-echo "Running tower2/html_to_pdf"
-
-docker run -i --rm --env PORT=3001 --cap-add=SYS_ADMIN -p 3001:3001/tcp --name html-to-pdf ifournight/html-to-pdf
-# docker run -d -i --rm --env PORT=3001 --cap-add=SYS_ADMIN -p 3001:3001/tcp --name html-to-pdf ifournight/html-to-pdf
